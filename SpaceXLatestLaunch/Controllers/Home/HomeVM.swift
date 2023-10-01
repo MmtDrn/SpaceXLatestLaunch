9fe468a0ceb70f891813b556525f087d951b2f46
mehmet duran <mmt.durann@gmail.com>
2023-10-01 09:20:58 +0300

    - fetch methods added

--- SpaceXLatestLaunch/Controllers/Home/HomeVM.swift
+++ SpaceXLatestLaunch/Controllers/Home/HomeVM.swift
@@ -9,12 +9,20 @@ import Foundation
 
 enum HomeVMStateChange: StateChange {
     case fetchSuccess
+    case crewFetchSuccess
+    case rocketFetchSuccess
+    case capsuleFetchSuccess
+    case launchpadFetchSuccess
     case showAlert(String)
 }
 
 class HomeVM: StatefulVM<HomeVMStateChange> {
     let dataSource = HomeDS()
     var networking: NetworkingProtocol
+    
+    private var launchesModel = LatestLaunchesModel()
+    private var crewArray = [CrewModel]()
+    private var capsuleArray = [CapsuleModel]()
     
     init(networking: NetworkingProtocol) {
         self.networking = networking
@@ -28,8 +36,94 @@ class HomeVM: StatefulVM<HomeVMStateChange> {
             if let error {
                 self.emit(.showAlert(error.errorMessage))
             } else if let response {
+                self.launchesModel = response
                 self.dataSource.launchesModel = response
+                self.fetchCrew()
+                self.fetchRocket()
+                self.fetchCapsule()
+                self.fetcLaunchpad()
                 self.emit(.fetchSuccess)
+            }
+        }
+    }
+    
+    private func fetchCrew() {
+        guard let crews = self.launchesModel.crew else { return }
+        let dispatchGroup = DispatchGroup()
+
+        for crew in crews {
+            dispatchGroup.enter()
+            networking.request(router: Routers.crew(id: crew)) { [weak self]
+                (response: CrewModel?, error) in
+                guard let self else { return }
+
+                if let error {
+                    self.emit(.showAlert(error.errorMessage))
+                } else if let response {
+                    self.crewArray.append(response)
+                }
+                dispatchGroup.leave()
+            }
+        }
+        dispatchGroup.notify(queue: .main) {
+            self.dataSource.crewArray = self.crewArray
+            self.emit(.crewFetchSuccess)
+        }
+    }
+    
+    private func fetchRocket() {
+        guard let rocket = self.launchesModel.rocket else { return }
+        
+        networking.request(router: Routers.rocket(id: rocket)) { [weak self]
+            (response: RocketModel?, error) in
+            guard let self else { return }
+            
+            if let error {
+                self.emit(.showAlert(error.errorMessage))
+            } else if let response {
+                self.dataSource.rocketModel = response
+                self.emit(.rocketFetchSuccess)
+            }
+        }
+    }
+    
+    private func fetchCapsule() {
+        guard let capsules = self.launchesModel.capsules else { return }
+        let dispatchGroup = DispatchGroup()
+        
+        for capsule in capsules {
+            dispatchGroup.enter()
+            
+            networking.request(router: Routers.capsules(id: capsule)) { [weak self]
+                (response: CapsuleModel?, error) in
+                guard let self else { return }
+                
+                if let error {
+                    self.emit(.showAlert(error.errorMessage))
+                } else if let response {
+                    self.capsuleArray.append(response)
+                }
+                dispatchGroup.leave()
+            }
+        }
+        dispatchGroup.notify(queue: .main) {
+            self.dataSource.capsuleArray = self.capsuleArray
+            self.emit(.capsuleFetchSuccess)
+        }
+    }
+    
+    private func fetcLaunchpad() {
+        guard let launchpad = self.launchesModel.launchPad else { return }
+        
+        networking.request(router: Routers.launchPads(id: launchpad)) {[weak self]
+            (response: LaunchPadsModel?, error) in
+            guard let self else { return }
+            
+            if let error {
+                self.emit(.showAlert(error.errorMessage))
+            } else if let response {
+                self.dataSource.launchpadModel = response
+                self.emit(.launchpadFetchSuccess)
             }
         }
     }
